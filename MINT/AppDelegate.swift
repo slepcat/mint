2ddc8ca2aecdf7a89da0e0a514b3abd656b13472
T.Ando <taizo.ando@gmail.com>
2014-10-15 23:53:22 +0900

    Initial Commit

---
+++ MINT/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MINT
+//
+//  Created by 安藤 泰造 on 2014/10/15.
+//  Copyright (c) 2014年 Taizo A. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
