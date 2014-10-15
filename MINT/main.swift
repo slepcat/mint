b62b8469bf5695c50486266cfb920de1106edd32
NemuNeko <slepcat@gmail.com>
2014-10-15 23:53:22 +0900

    Initial Commit

---
+++ MINT/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MINT
+//
+//  Created by 安藤 泰造 on 2014/10/15.
+//  Copyright (c) 2014年 Taizo A. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
---
+++ MINTTests/MINTTests.swift
@@ -0,0 +1,36 @@
+//
+//  MINTTests.swift
+//  MINTTests
+//
+//  Created by 安藤 泰造 on 2014/10/15.
+//  Copyright (c) 2014年 Taizo A. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class MINTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
