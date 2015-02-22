31752aee29e9ba7216989ae64388ab549a48c152
T.Ando <taizo.ando@gmail.com>
2015-02-22 14:22:28 +0900

    1. Start implementation of openJSCAD (Vector & Vertex Struct) 2. Tag factory class move to new file(tag.swift) because of access control

---
+++ MINT/tag.swift
@@ -0,0 +1,30 @@
+//
+//  tag.swift
+//  MINT
+//
+//  Created by 安藤 泰造 on 2015/02/22.
+//  Copyright (c) 2015年 Taizo A. All rights reserved.
+//
+
+import Foundation
+
+
+// tag generator.
+// Tag factory: we can request a unique tag through Tag.getTag
+// Singleton
+
+class Tag {
+    private var count:Int = 0
+    private init(){}
+    
+    var newTag: Int {
+        return count++
+    }
+    
+    class var get: Tag {
+        struct Static{
+            static let tagFactory = Tag()
+        }
+        return Static.tagFactory
+    }
+}
