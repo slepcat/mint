61e1b4fcb97a5eb4e7ac545f5749cd357fe17fb1
NemuNeko <slepcat@gmail.com>
2015-03-02 23:19:07 +0900

    1. Modern OpenGL implementation using glDrawArray(). Success to draw a triangle. But it will be changed to glDrawElements() using VBO. 2. protocol is just a sketch.

---
+++ MINT/MintProtocols.swift
@@ -0,0 +1,25 @@
+//
+//  MintProtocols.swift
+//  MINT
+//
+//  Created by 安藤 泰造 on 2015/02/22.
+//  Copyright (c) 2015年 Taizo A. All rights reserved.
+//
+
+import Foundation
+
+protocol MintObserver {
+    func update(mesh:[Vertex])
+}
+
+protocol MintSubject {
+    var mesh: [Vertex] {get set}
+    var bufferid:Int {get set}
+    var updated: Bool {get set}
+    func registerObserver(observer: MintObserver)
+    func removeObserver(observer: MintObserver)
+}
+
+protocol MintClass {
+    
+}
